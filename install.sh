f04e3190c2c5cf00cd24926fccd0fb7556436339
MoonXu0722 <xpylovely520@163.com>
2022-08-19 17:34:02 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,5 +1,6 @@
-sudo apt install libc-ares-dev libev-dev libpcre3 libpcre3-dev libmbedtls-dev
-git clone https://github.com/MoonXu0722/shadowsocks-dependency-package.git
+#/bin/sh
+sudo apt install libc-ares-dev libev-dev libpcre3 libpcre3-dev libmbedtls-dev build-essential
+#git clone https://github.com/MoonXu0722/shadowsocks-dependency-package.git
 cd shadowsocks-dependency-package
 tar xvf libsodium-1.0.18-stable.tar.gz
 cd libsodium-stable 
@@ -12,3 +13,10 @@ cd shadowsocks-libev-3.3.5
 ./configure --disable-documentation
 make
 sudo make install
+mkdir /etc/shadowsocks
+cp multi_config.json /etc/shadowsocks
+cp ssmanager.service /etc/systemd/system/
+
+#sudo passwd root 1 1
+systemctl enable ssmanager
+systemctl start ssmanager
